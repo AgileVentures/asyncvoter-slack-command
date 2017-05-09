1c6dc564c7a463241258bb169236bc0256a82717
Raphael Krausz <krauszraphael@gmail.com>
2017-05-09 15:46:57 +0200

    Added in winston and applied logs

--- async-voter.js
+++ async-voter.js
@@ -3,14 +3,19 @@ const request = require('request')
 const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 
+const winston = require('winston')
+
 module.exports = (app, repository) => {
 
   app.get('/', (req, res) => {
+    winston.info('Received GET /')
     res.render('index', { client_id: clientId })
   })
 
   app.get('/oauth', (req, res) => {
+    winston.info('Received GET /oauth')
     if (!req.query.code) {
+      winston.error('Received GET /oauth - but no code received')
       res.status(500)
       res.send({ 'Error': "Looks like we're not getting code." })
     } else {
@@ -20,9 +25,11 @@ module.exports = (app, repository) => {
         method: 'GET'
       }, (error, response, body) => {
         if (error) {
+          winston.error('Received GET /oauth - but an error occurred. ' + error)
           res.status(500)
           res.send({ 'Error': error })
         } else {
+          winston.info('Received GET /oauth - processed successfully')
           res.redirect('/')
         }
       })
@@ -33,10 +40,28 @@ module.exports = (app, repository) => {
     const text = req.body.text
     const channel_id = req.body.channel_id
 
+    winston.info('Received POST /commands')
+    winston.info('channel_id: ' + channel_id)
+    winston.info('text: ' + text)
+
     // TODO: Close previous session. One session per channel is allowed.
     repository.del(channel_id, (err, reply) => {
       // TODO: Save unique voting session. Team + Channel
+      if (err) {
+        winston.error('Repository error deleting channel_id: ' + channel_id)
+        winston.error('Error: ' + err)
+      } else {
+        winston.info('Repository deleted channel_id: ' + channel_id)
+        winston.info('Repository reply: ' + reply)
+      }
       repository.set(channel_id, JSON.stringify([]), (err, reply) => {
+        if (err) {
+          winston.error('Repository error deleting channel_id: ' + channel_id)
+          winston.error('Error: ' + err)
+        } else {
+          winston.info('Repository blanked channel_id: ' + channel_id)
+          winston.info('Repository reply: ' + reply)
+        }
         res.send(formatStart(text))
       })
     })
@@ -50,8 +75,23 @@ module.exports = (app, repository) => {
     const user = payload.user
     const channel_id = payload.channel.id
 
+    winston.info("Received POST /actions")
+    winston.info('text: ' + text)
+    winston.info('channel_id: ' + channel_id)
+    winston.info('user: ' + JSON.stringify(user))
+
     repository.get(channel_id, (err, reply) => {
+      if (err) {
+        winston.error("Repository error getting channel_id: " + channel_id)
+        winston.error("Error: " + error)
+      } else {
+        winston.info("Repository got channel_id: " + channel_id)
+        winston.info("Repository reply: " + reply)
+      }
+
       const votes = JSON.parse(reply)
+
+      winston.info('actions[0].value: ' + actions[0].value)
 
       if (actions[0].value === 'reveal') {
         res.send(formatResult(text, votes))
@@ -59,7 +99,19 @@ module.exports = (app, repository) => {
         // TODO: Count vote for different voting sessions
         votes.push({ 'user': user, 'vote': actions[0].value })
 
-        repository.set(channel_id, JSON.stringify(votes), (err, reply) => {
+        const jsonVotes = JSON.stringify(votes)
+
+        repository.set(channel_id, jsonVotes, (err, reply) => {
+          if (err) {
+            winston.error("Repository error setting channel_id: " + channel_id)
+            winston.error("Votes were: " + jsonVotes)
+            winston.error("Error: " + error)
+          } else {
+            winston.info("Repository set channel_id: " + channel_id)
+            winston.info("Votes were: " + jsonVotes)
+            winston.info("Repository reply: " + reply)
+          }
+
           res.send(formatRegister(text, votes))
         })
       }
--- index.js
+++ index.js
@@ -2,13 +2,33 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const winston = require('winston')
 const redis = require('redis')
-
 const db = redis.createClient(process.env.REDIS_URL || 'redis://localhost:6379')
 
 db.on('error', function (err) {
+  winston.error('Redis error: ' + err)
   console.log('Error ' + err)
+})
+
+db.on('ready', () => {
+  winston.info("Redis connection ready")
+})
+
+db.on('connect', () => {
+  winston.info("Redis connected")
+})
+
+db.on('reconnect', (message) => {
+  winston.info("Redis reconnecting: " + message)
+})
+
+db.on('end', () => {
+  winston.info("Redis connection closed")
+})
+
+db.on('warning', (warning) => {
+  winston.warn("Redis warning received: " + warning)
 })
 
 const PORT = process.env.PORT || 4390
@@ -23,7 +43,7 @@ app.use(bodyParser.json())
 require('./async-voter')(app, db)
 
 const server = app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT)
+  winston.info('Listening on port ' + PORT)
 })
 
 module.exports = { app, server, db }
