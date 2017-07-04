f7cf68d2da941283f561adc21fbd384543743a09
Raphael Krausz <krauszraphael@gmail.com>
2017-07-04 04:14:05 +0200

    Commented out test helper module for the time being

--- test/helpers/integrationHelper.js
+++ test/helpers/integrationHelper.js
@@ -1,24 +1,24 @@
-module.exports = (chai, app) => {
+// module.exports = (chai, app) => {
 
-  // Makes a promise to the response text of a vote cast via HTTP
-  function makeVote(channelId, voteLabel, username, actionValue) {
+    //   // Makes a promise to the response text of a vote cast via HTTP
+    //   function makeVote(channelId, voteLabel, username, actionValue) {
 
-    console.log("Here I am")
+    //     console.log("Here I am")
 
-    return chai.request(app)
-      .post('/actions')
-      .send({
-        payload: JSON.stringify({
-          channel: { id: channelId },
-          actions: [{ value: actionValue }],
-          user: { name: username },
-          original_message: { text: voteLabel }
-        })
-      })
-      .catch(err => {
-        console.log("!!!! ERR:", err)
-      })
-  }
+    //     return chai.request(app)
+    //       .post('/actions')
+    //       .send({
+    //         payload: JSON.stringify({
+    //           channel: { id: channelId },
+    //           actions: [{ value: actionValue }],
+    //           user: { name: username },
+    //           original_message: { text: voteLabel }
+    //         })
+    //       })
+    //       .catch(err => {
+    //         console.log("!!!! ERR:", err)
+    //       })
+    //   }
 
-  return { makeVote }
-}
+    //   return { makeVote }
+    // }
